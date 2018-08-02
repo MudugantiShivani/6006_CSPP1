3dc9eb33939573e533e3c23e975d8127848ee9cc
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-02 18:56:01 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -20,5 +20,5 @@ def main():
     print(count_i)
     # the input string is in s
     # remove pass and start your code here
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
