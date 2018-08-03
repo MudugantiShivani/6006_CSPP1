2834bb573b4ce2b458fe4687e89434f0fce6e179
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-03 14:41:56 +0530

    " -> 0 of 4 passed. pylint: 7.86/10 "

---
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -0,0 +1,33 @@
+# Write a python program to find if the given number is a perfect cube or not
+# using guess and check algorithm
+
+# testcase 1
+# Input: 24389
+# Output: 24389 is a perfect cube
+
+# testcase 2
+# Input: 21950
+# Output: 21950 is not a perfect cube
+
+def main():
+    # input is captured in s
+    cube_num=int(input())
+    epsilon = 0.01
+    inc = 1
+    guess = 0
+
+    while guess <= cube_num:
+        if abs(guess**3 -cube_num) < epsilon:
+            break
+        else:
+            guess += inc
+
+    if abs(guess**3 - cube_num) >= epsilon:
+        print('failed')
+    else:
+        print(str(guess))
+    # watch out for the data type of value stored in s
+    # your code starts here
+
+if __name__ == "__main__":
+    main()
