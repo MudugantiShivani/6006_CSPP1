e726250faa6b4dbfeac98256c23bc8e7c2e9df07
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-03 15:39:25 +0530

    " -> 0 of 1 passed. pylint: 5.71/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -2,22 +2,22 @@
 # using approximation method
 
 def main():
-	x=int(input())
-	epsilon = 0.01
-	step = 0.1
-	guess = 0.0
+    x=int(input())
+    epsilon = 0.01
+    step = 0.1
+    guess = 0.0
 
-	while abs(guess**2-x) >= epsilon:
-    	if guess <= x:
-       		guess += step
-    	else:
-        	break
+    while abs(guess**2-x) >= epsilon:
+        if guess <= x:
+            guess += step
+        else:
+            break
 
-	if abs(guess**2 - x) >= epsilon:
-    	print('failed')
-	else:
-    	print('succeeded: ' + str(guess))
-	#your code here
+    if abs(guess**2 - x) >= epsilon:
+        print('failed')
+    else:
+        print('succeeded: ' + str(guess))
+    #your code here
 
 if __name__ == "__main__":
-	main()
+    main()
