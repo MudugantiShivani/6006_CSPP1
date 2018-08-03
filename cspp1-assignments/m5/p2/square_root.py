482ddfd5cef8eb934619df1be5969ae0b562b5e3
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-03 18:11:37 +0530

    " -> 3 of 3 passed. pylint: 6.43/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -1,5 +1,13 @@
-# Write a python program to find the square root of the given number 
+# Write a python program to find the square root of the given number
 # using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
 
 def main():
     x=int(input())
@@ -16,8 +24,10 @@ def main():
     if abs(guess**2 - x) >= epsilon:
         print('failed')
     else:
-        print('succeeded: ' + str(guess))
-    #your code here
+        print(str(guess))
+    # epsilon and step are initialized
+    # don't change these values
+    # your code starts here
 
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
