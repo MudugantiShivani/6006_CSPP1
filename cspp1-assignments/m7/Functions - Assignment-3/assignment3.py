d370c03c13880f9141a016babee8795c28003550
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-06 20:23:30 +0530

    " -> 0 of 10 passed. pylint: 7.20/10 "

--- cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
@@ -5,31 +5,31 @@
 # value as you did in Assignment 2.
 '''
 def payingdebt_offinayear(balance_in, annual_interestrate):
-	'''In this program we will give the input as the balance available
-	and annual interest rate
-	'''
-	init_balance = balance_in
-	moninterest_rate = annual_interestrate/12
-	low_i=init_balance/12
-	up_i = (init_balance * (1+ moninterest_rate)**12)/12.0
-	epsilon = 0.03
-	while abs(balance_in) > epsilon:
-		mon_payrate = (up_i+low_i)/2
-		balance_in = init_balance
-		for _ in range(12):
-			ans_i = balance_in - mon_payrate
-			balance_in = ans_i + (ans_i * moninterest_rate)
-		if balance_in > epsilon:
-			low_i = mon_payrate
-		elif balance_in < -epsilon:
-			up_i = mon_payrate
-		else:
-			break
-	return str(round(mon_payrate, 2))
+    '''In this program we will give the input as the balance available
+    and annual interest rate
+    '''
+    init_balance = balance_in
+    moninterest_rate = annual_interestrate/12
+    low_i=init_balance/12
+    up_i = (init_balance * (1+ moninterest_rate)**12)/12.0
+    epsilon = 0.03
+    while abs(balance_in) > epsilon:
+        mon_payrate = (up_i+low_i)/2
+        balance_in = init_balance
+        for _ in range(12):
+            ans_i = balance_in - mon_payrate
+            balance_in = ans_i + (ans_i * moninterest_rate)
+        if balance_in > epsilon:
+            low_i = mon_payrate
+        elif balance_in < -epsilon:
+            up_i = mon_payrate
+        else:
+            break
+    return str(round(mon_payrate, 2))
 
 def main():
-	'''find the lowest amount to be paid to make the interest zero
-	'''
+    '''find the lowest amount to be paid to make the interest zero
+    '''
     data = input()
     # data = "4773 0.2"
     data = data.split(' ')
