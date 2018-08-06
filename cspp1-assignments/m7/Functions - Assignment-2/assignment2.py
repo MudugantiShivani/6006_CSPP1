9748efacee192f167b32e7bc15f9ecdaf5227eab
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-06 21:01:07 +0530

    " -> 10 of 10 passed. pylint: 6.67/10 "

--- cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
+++ cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
@@ -16,31 +16,38 @@
 # made).
 
 def payingdebt_offinayear(Previous_balance, Annual_interestrate):
-	Minimumfixed_monthlypayment = 10
-	temp = Previous_balance
-	flag = 1
-	mon_cnt = 0
-	monthly_interestrate = (Annual_interestrate) / 12.0
-	if Previous_balance < 0:
-		flag = 0
-		Minimumfixed_monthlypayment = 0
-	while flag:
-		mon_cnt += 1
-		monthly_unpaidbalance = (Previous_balance) - (Minimumfixed_monthlypayment)
-		Previous_balance = (monthly_unpaidbalance) + (monthly_interestrate * monthly_unpaidbalance)
-		if Previous_balance < 0:
-			flag = 0
-		if mon_cnt == 12 and flag != 0:
-			Minimumfixed_monthlypayment += 10
-			mon_cnt = 0
-			Previous_balance = temp
-	return Minimumfixed_monthlypayment
+    '''
+    Here we will give the previous balance and annual interest rate to
+    find the minimum amount
+    '''
+    Minimumfixed_monthlypayment = 10
+    temp = Previous_balance
+    flag = 1
+    mon_cnt = 0
+    monthly_interestrate = (Annual_interestrate) / 12.0
+    if Previous_balance < 0:
+        flag = 0
+        Minimumfixed_monthlypayment = 0
+    while flag:
+        mon_cnt += 1
+        monthly_unpaidbalance = (Previous_balance) - (Minimumfixed_monthlypayment)
+        Previous_balance = (monthly_unpaidbalance) + (monthly_interestrate * monthly_unpaidbalance)
+        if Previous_balance < 0:
+            flag = 0
+        if mon_cnt == 12 and flag != 0:
+            Minimumfixed_monthlypayment += 10
+            mon_cnt = 0
+            Previous_balance = temp
+    return Minimumfixed_monthlypayment
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	print("Lowest Payment:",payingdebt_offinayear(data[0],data[1]))
-	
+    '''
+    this function takes the in puts and calls payingdebt_offinayear
+    '''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print("Lowest Payment:",payingdebt_offinayear(data[0],data[1]))
+    
 if __name__ == "__main__":
-	main()
-	
+    main()
+    
