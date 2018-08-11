8597f30317e7c083725683eea2d2e4f950d21437
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-11 11:54:16 +0530

    " -> 0 of 3 passed. pylint: 9.41/10 "

--- cspp1-assignments/m12/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/create_social_network.py
@@ -39,9 +39,9 @@ def main():
     '''
     main function is written here
     '''
-    n = input()
+    n_input = input()
     data = {}
-    for input_i in range(int(n)):
+    for input_i in range(int(n_input)):
         s_string = input()
         l_len = s_string.split('follows')
         if l_len[0] not in data:
