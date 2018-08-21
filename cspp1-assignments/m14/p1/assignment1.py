c2cee2a7add4103505934133d82a75f19a5f7ccd
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-21 23:45:20 +0530

    " -> 5 of 5 passed. pylint: 9.76/10 "

--- cspp1-assignments/m14/p1/assignment1.py
+++ cspp1-assignments/m14/p1/assignment1.py
@@ -151,8 +151,6 @@ class Message(object):
             else:
                 new_msg.append(self.build_shift_dict(shift)[i])
         return ''.join(new_msg)
-        
-
 def main():
     '''
         Function to handle testcases
