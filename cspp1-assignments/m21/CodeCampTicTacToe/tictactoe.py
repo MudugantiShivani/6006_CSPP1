a50cbfcb014f1ced18cabf4dc710e148cc113396
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-24 22:05:27 +0530

    " -> 14 of 14 passed. pylint: 9.23/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -1,11 +1,13 @@
 '''
-Finding the winner of tic tac toe game by writing different functions
+    tictactoe game possibilities has to be checked and return the winner or invalid input or invalid game or draw
 '''
-
 def tictactoe(game):
+    '''
+    writing all the possibilities and checking them.
+    '''
     flag = 0
     count = 0
-    if  (game[0][0] == game[1][0] == game[2][0] == "x"
+    if (game[0][0] == game[1][0] == game[2][0] == "x"
             or game[0][1] == game[1][1] == game[2][1] == "x"
             or game[0][2] == game[1][2] == game[2][2] == "x"
             or game[0][0] == game[0][1] == game[0][2] == "x"
@@ -15,7 +17,7 @@ def tictactoe(game):
             or game[0][2] == game[1][1] == game[2][0] == "x"):
         flag = 1
         count += 1
-    if  (game[0][0] == game[1][0] == game[2][0] == "o"
+    if (game[0][0] == game[1][0] == game[2][0] == "o"
             or game[0][1] == game[1][1] == game[2][1] == "o"
             or game[0][2] == game[1][2] == game[2][2] == "o"
             or game[0][0] == game[0][1] == game[0][2] == "o"
@@ -26,38 +28,38 @@ def tictactoe(game):
         flag = 2
         count += 1
     if count == 2:
-        return "invalid game"
+        return 'invalid game'
     if flag == 1:
-        return "x"
+        return 'x'
     if flag == 2:
-        return "o"
-    return "draw"
+        return 'o'
+    return 'draw'
 def main():
     '''
-    giving input to a program
+    we give input to the program and print output depending on the input.
     '''
-    tictac = []
+    game = []
     flag = 0
-    count_x = 0
-    count_o = 0
+    count_of_of_x = 0
+    count_of_o = 0
     for i in range(3):
-        tictac.append(input().split())
+        game.append(input().split())
     for i in range(3):
         for j in range(3):
-            if tictac[i][j] == "x" or tictac[i][j] == "o" or tictac[i][j] == ".":
+            if game[i][j] == "x" or game[i][j] == "o" or game[i][j] == ".":
                 flag = 1
             else:
                 flag = 0
-            if tictac[i][j] == "x":
-                count_x += 1
-            elif tictac[i][j] == "o":
-                count_o += 1
+            if game[i][j] == "x":
+                count_of_of_x += 1
+            elif game[i][j] == "o":
+                count_of_o += 1
     if flag == 0:
         print("invalid input")
-    elif count_x or count_o >5:
+    elif count_of_of_x > 5 or count_of_o > 5:
         print("invalid game")
     else:
-        print(tictactoe(tictac))
+        print(tictactoe(game))
 if __name__ == '__main__':
     main()
-
+    
