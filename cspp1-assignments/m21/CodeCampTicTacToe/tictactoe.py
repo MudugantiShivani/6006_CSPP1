f106d5381376bd06df6d15b316772ca81a18daf2
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-24 21:57:08 +0530

    " -> 5 of 14 passed. pylint: 5.13/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -54,10 +54,10 @@ def main():
                 count_o += 1
     if flag == 0:
         print("invalid input")
-    elif count_x > 5 or count_o > 5:
+    elif count_x or count_o >5:
         print("invalid game")
     else:
-        print(mat(tictac))
+        print(tictactoe(tictac))
 if __name__ == '__main__':
     main()
 
