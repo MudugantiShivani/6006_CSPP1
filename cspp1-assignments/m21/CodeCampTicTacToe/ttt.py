f47fe55f803bf72e11164f0b2787d0a38c8ef22b
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-24 22:00:07 +0530

    " -> 14 of 14 passed. pylint: 9.23/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/ttt.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/ttt.py
@@ -1,64 +1,64 @@
 '''
     finding who's is the winner
 '''
-def mat(matrix):
+def tictactoe(game):
     '''
     checking the possibilities
     '''
-    temp = 0
+    flag = 0
     count = 0
-    if (matrix[0][0] == matrix[1][0] == matrix[2][0] == "x"
-            or matrix[0][1] == matrix[1][1] == matrix[2][1] == "x"
-            or matrix[0][2] == matrix[1][2] == matrix[2][2] == "x"
-            or matrix[0][0] == matrix[0][1] == matrix[0][2] == "x"
-            or matrix[1][0] == matrix[1][1] == matrix[1][2] == "x"
-            or matrix[2][0] == matrix[2][1] == matrix[2][2] == "x"
-            or matrix[0][0] == matrix[1][1] == matrix[2][2] == "x"
-            or matrix[0][2] == matrix[1][1] == matrix[2][0] == "x"):
-        temp = 1
+    if (game[0][0] == game[1][0] == game[2][0] == "x"
+            or game[0][1] == game[1][1] == game[2][1] == "x"
+            or game[0][2] == game[1][2] == game[2][2] == "x"
+            or game[0][0] == game[0][1] == game[0][2] == "x"
+            or game[1][0] == game[1][1] == game[1][2] == "x"
+            or game[2][0] == game[2][1] == game[2][2] == "x"
+            or game[0][0] == game[1][1] == game[2][2] == "x"
+            or game[0][2] == game[1][1] == game[2][0] == "x"):
+        flag = 1
         count += 1
-    if (matrix[0][0] == matrix[1][0] == matrix[2][0] == "o"
-            or matrix[0][1] == matrix[1][1] == matrix[2][1] == "o"
-            or matrix[0][2] == matrix[1][2] == matrix[2][2] == "o"
-            or matrix[0][0] == matrix[0][1] == matrix[0][2] == "o"
-            or matrix[1][0] == matrix[1][1] == matrix[1][2] == "o"
-            or matrix[2][0] == matrix[2][1] == matrix[2][2] == "o"
-            or matrix[0][0] == matrix[1][1] == matrix[2][2] == "o"
-            or matrix[0][2] == matrix[1][1] == matrix[2][0] == "o"):
-        temp = 2
+    if (game[0][0] == game[1][0] == game[2][0] == "o"
+            or game[0][1] == game[1][1] == game[2][1] == "o"
+            or game[0][2] == game[1][2] == game[2][2] == "o"
+            or game[0][0] == game[0][1] == game[0][2] == "o"
+            or game[1][0] == game[1][1] == game[1][2] == "o"
+            or game[2][0] == game[2][1] == game[2][2] == "o"
+            or game[0][0] == game[1][1] == game[2][2] == "o"
+            or game[0][2] == game[1][1] == game[2][0] == "o"):
+        flag = 2
         count += 1
     if count == 2:
         return 'invalid game'
-    if temp == 1:
+    if flag == 1:
         return 'x'
-    if temp == 2:
+    if flag == 2:
         return 'o'
     return 'draw'
 def main():
     '''
     giving input to a program
     '''
-    matrix = []
+    game = []
     flag = 0
-    count_x = 0
-    count_o = 0
+    count_of_of_x = 0
+    count_of_o = 0
     for i in range(3):
-        matrix.append(input().split())
+        game.append(input().split())
     for i in range(3):
         for j in range(3):
-            if matrix[i][j] == "x" or matrix[i][j] == "o" or matrix[i][j] == ".":
+            if game[i][j] == "x" or game[i][j] == "o" or game[i][j] == ".":
                 flag = 1
             else:
                 flag = 0
-            if matrix[i][j] == "x":
-                count_x += 1
-            elif matrix[i][j] == "o":
-                count_o += 1
+            if game[i][j] == "x":
+                count_of_of_x += 1
+            elif game[i][j] == "o":
+                count_of_o += 1
     if flag == 0:
         print("invalid input")
-    elif count_x > 5 or count_o > 5:
+    elif count_of_of_x > 5 or count_of_o > 5:
         print("invalid game")
     else:
-        print(mat(matrix))
+        print(tictactoe(game))
 if __name__ == '__main__':
     main()
