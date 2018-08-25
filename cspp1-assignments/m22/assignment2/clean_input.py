29b3e2769c79278669327225e52a5dc684564864
MudugantiShivani <shivani.muduganti72@msitprogram.net>
2018-08-25 11:33:16 +0530

    " -> 3 of 3 passed. pylint: 8.75/10 "

--- cspp1-assignments/m22/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -3,12 +3,13 @@ Write a function to clean up a given string by removing the special characters a
 alphabets in both upper and lower case and numbers.
 '''
 def main():
+    '''
+    here we will take a string and remove all special characters from it and print the string
+    '''
     string = input()
     for char in string:
         if char in('!', '@', '#', '$', '%', '^', '&', '*', '.', ' ', '(', ')'):
             string = string.replace(char, "")
     print(string)
-
-
 if __name__ == '__main__':
     main()
